8f7d14e8408c4482b622eb1ea45282be517131be
PeterDaniel333 <dani00194@gmail.com>
2023-01-26 09:54:56 +0100

    first commit

---
+++ asd.py
@@ -0,0 +1,6 @@
+m = (input("Irj be egy vezeteknevet: "))
+z = (input("Irj be egy utonevet: "))
+x = (input("Irj be egy vezeteknevet: "))
+y = (input("Irj be egy utonevet: "))
+
+print(f'lehetseges nevek: {m} {z}, {x} {y}, {m} {y}, {x} {z}')
